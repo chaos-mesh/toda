91cc306ede6d0cf2d3e82d35ac0b558c0ff49a15
Yang Keao <keao.yang@yahoo.com>
2020-08-17 13:27:29 +0800

    ignore existing fuse
    Signed-off-by: Yang Keao <keao.yang@yahoo.com>

--- src/fuse_device.rs
+++ src/fuse_device.rs
@@ -1,4 +1,5 @@
 use nix::sys::stat::{stat, mknod, SFlag, Mode};
+use nix::Error as NixError;
 
 pub fn read_fuse_dev_t() -> anyhow::Result<u64> {
     let fuse_stat = stat("/dev/fuse")?;
@@ -8,7 +9,17 @@ pub fn read_fuse_dev_t() -> anyhow::Result<u64> {
 
 pub fn mkfuse_node(dev: u64) -> anyhow::Result<()> {
     let mode = unsafe {Mode::from_bits_unchecked(0o666)};
-    mknod("/dev/fuse", SFlag::S_IFCHR, mode, dev)?;
-
-    Ok(())
+    match mknod("/dev/fuse", SFlag::S_IFCHR, mode, dev) {
+        Ok(()) => Ok(()),
+        Err(NixError::Sys(errno)) => {
+            if errno == nix::errno::Errno::EEXIST {
+                Ok(())
+            } else {
+                Err(NixError::from_errno(errno).into())
+            }
+        }
+        Err(err) => {
+            Err(err.into())
+        }
+    }
 }
