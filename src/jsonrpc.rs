b740a2d95078d4e2e20b758bba81665ca72a9364
AsterNighT <klxjt99@outlook.com>
2021-03-11 13:43:15 +0800

    Add some tracing output and a test
    Signed-off-by: AsterNighT <klxjt99@outlook.com>

--- src/jsonrpc.rs
+++ src/jsonrpc.rs
@@ -1,20 +1,25 @@
 use jsonrpc_stdio_server::{ServerBuilder, jsonrpc_core::*};
-use tracing::info;
+use tracing::{info, trace};
 
 pub async fn start_server() {
     info!("Starting jsonrpc server");
     let server = new_server();
-    server.build().await;
+    let server = server.build();
+    server.await;
 }
 
 pub fn new_server() -> ServerBuilder {
     let mut io = IoHandler::default();
     handle_ping(&mut io);
+    let request = r#"{"jsonrpc": "2.0", "method": "ping", "params": {}, "id": 1}"#;
+	let response = r#"{"jsonrpc":"2.0","result":"pong","id":1}"#;
+    assert_eq!(io.handle_request_sync(request), Some(response.to_string()));
     return ServerBuilder::new(io)
 }
 
 fn handle_ping(handler:&mut IoHandler) {
     handler.add_sync_method("ping", |_params|{
+        trace!("Receiving ping");
         Ok(Value::String("pong".to_owned()))
     });
 }
