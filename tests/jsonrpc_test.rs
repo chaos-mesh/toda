0ed7799984655739ae5ef46f022a17d502b837de
AsterNighT <klxjt99@outlook.com>
2021-03-12 10:48:52 +0800

    Update test to make it work like a go client
    Signed-off-by: AsterNighT <klxjt99@outlook.com>

--- tests/jsonrpc_test.rs
+++ tests/jsonrpc_test.rs
@@ -2,7 +2,7 @@ use toda::jsonrpc::new_handler;
 #[test]
 fn test_ping() {
     let io = new_handler();
-    let request = r#"{"jsonrpc": "2.0", "method": "ping", "params": null, "id": 1}"#;
+    let request = r#"{"jsonrpc": "2.0","method":"ping","id":1}"#;
     let response = r#"{"jsonrpc":"2.0","result":"pong","id":1}"#;
     assert_eq!(io.handle_request_sync(request), Some(response.to_string()));
 }
